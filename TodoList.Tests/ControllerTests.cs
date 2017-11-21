80fed58dd84721b4ec0857c7fb33dee9b2e4f811
Bob Murray <murray395@gmail.com>
2017-11-21 11:50:25 -0500

    Add as test

---
+++ TodoList.Tests/ControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoList.Controllers;
+using TodoList.Repositories;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using TodoList.Models;
+
+namespace TodoList.Tests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        HomeController homeController;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var repository = new TodoInMemoryRepository();
+            homeController = new HomeController(repository);
+        }
+
+        [TestMethod]
+        public void IndexControllerTest()
+        {
+            var viewResult = homeController.Index() as ViewResult;
+
+            var model = viewResult.ViewData.Model as List<Todo>;
+
+            Assert.IsTrue(model.Count > 0);
+        }
+    }
+}
---
+++ TodoList.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TodoList.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TodoList.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d591e185-af08-444d-b79a-ce0c3314bab8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
